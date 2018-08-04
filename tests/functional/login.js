25a79e9e9d1a33f78ec802c04f4aed94997496d4
Martijn Jacobs <martijn@devopsconsulting.nl>
2018-08-04 14:51:49 +0200

    Addeda logout test

--- tests/functional/login.js
+++ tests/functional/login.js
@@ -4,10 +4,9 @@ const assert = require('chai').assert;
 const url = 'http://localhost:3333/';
 
 
+describe('Functional tests for login and logout', function() {
 
-describe('Login tests', function() {
-
-  this.timeout('30s');
+  this.timeout('300s');
 
   let browser = null;
 
@@ -25,56 +24,80 @@ describe('Login tests', function() {
       .goto(url);
   });
 
-  describe('Functional tests for login', () => {
-  
-    it('we should be redirected to the login page', done => {
-      browser
-        .end()
-        .evaluate( () => { return document; } )
-        .then(function (document) { 
-          assert.equal(document.location.href, 'http://localhost:3333/login');
-          done();
-        })
-        .catch(done);
-    });
-
-    it('we cannot login with false credentials', done => {
-      var selector = '.notice-wrapper';
-      browser
-        .type('#username', 'sald')
-        .type('#password', 'sald')
-        .click('#login-submit')
-        .wait('.notice')
-        .end()
-        .evaluate(selector => {
-          return document.querySelector('.notice-wrapper div').textContent;
-        }, selector)
-        .then(function (message) {
-          assert.equal(message, 'Authentication failed');
-          done() ;
-        })
-        .catch(done);
-    });
-
-    it('valid credentials will redirect us to the homepage and hide the loginform', done => {
-      browser
-        .type('#username', 'salt')
-        .type('#password', 'salt')
-        .click('#login-submit')
-        .wait( ()=> {
-          // we wait here for the loginpage to be hidden
-          var loginpage = document.querySelector('#page_login');
-          return loginpage.style.display == 'none';
-        })
-        .end()
-        .evaluate( ()=> { return document.location.href; })
-        .then(function (href) {
-          assert.equal(href, url);
-          done();
-        })
-        .catch(done);
-    });
-
+  it('we should be redirected to the login page', done => {
+    browser
+      .end()
+      .evaluate( () => { return document.location.href; })
+      .then(function (href) { 
+        assert.equal(href, 'http://localhost:3333/login');
+        done();
+      })
+      .catch(done);
   });
+
+  it('we cannot login with false credentials', done => {
+    var selector = '.notice-wrapper';
+    browser
+      .type('#username', 'sald')
+      .type('#password', 'sald')
+      .click('#login-submit')
+      .wait('.notice')
+      .end()
+      .evaluate(selector => {
+        return document.querySelector('.notice-wrapper div').textContent;
+      }, selector)
+      .then(function (message) {
+        assert.equal(message, 'Authentication failed');
+        done() ;
+      })
+      .catch(done);
+  });
+
+  it('valid credentials will redirect us to the homepage and hide the loginform', done => {
+    browser
+      .type('#username', 'salt')
+      .type('#password', 'salt')
+      .click('#login-submit')
+      .wait( ()=> {
+        // we wait here for the loginpage to be hidden
+        var loginpage = document.querySelector('#page_login');
+        return loginpage.style.display == 'none';
+      })
+      .end()
+      .evaluate( ()=> { return document.location.href; })
+      .then(function (href) {
+        assert.equal(href, url);
+        done();
+      })
+      .catch(done);
+  });
+
+  it('check that we can logout', done => {
+    browser
+      .type('#username', 'salt')
+      .type('#password', 'salt')
+      .click('#login-submit')
+      .wait( ()=> {
+        // we wait here for the loginpage to be hidden
+        var loginpage = document.querySelector('#page_login');
+        return loginpage.style.display == 'none';
+      })
+      .click('#button_logout')
+      .wait( ()=> {
+        // we wait here for the loginpage to be shown
+        var loginpage = document.querySelector('#page_login');
+        console.log(loginpage.style.display);
+        return loginpage.style.display === '';
+      })
+      .end()
+      .evaluate( ()=> { return document.location.href; })
+      .then(function (href) {
+        // and we a redirected to the login page
+        assert.equal(href,'http://localhost:3333/login');
+        done();
+      })
+      .catch(done);
+  });
+
 
 });
